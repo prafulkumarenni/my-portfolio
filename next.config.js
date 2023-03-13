17907104aff492e40f7865ae886d9a7e148e4c2b
prafulkumarenni <prafulkumarenni90@gmail.com>
2023-03-13 14:19:58 -0500

    next images issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "my-portfolio-git-master-prafulkumarenni.vercel.app",
+      "my-portfolio-prafulkumarenni.vercel.app",
+      "my-portfolio-brown-five.vercel.app",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
